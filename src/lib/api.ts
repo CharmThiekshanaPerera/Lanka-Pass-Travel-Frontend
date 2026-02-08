c41ae831eafe783067bc71bd59b1d3000879ad9a
Thiekshana Perera <thiekshana@phyxle.com>
2026-02-09 00:14:07 +0530

    update

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -3,7 +3,8 @@ import axios from 'axios';
 
 // Create axios instance
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
+  //  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8000',
+  baseURL: import.meta.env.VITE_API_URL || 'https://13.212.50.145',
   headers: {
     'Content-Type': 'application/json',
   },
